6230444ac2d1fc0da891afcda7fdfe839ea02c28
ad030 <68517956+ad030@users.noreply.github.com>
2026-05-23 09:56:19 -0400

    added nixpkgs unstable to flake in case i want to use it (like for neovim)

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-    # nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -12,10 +12,18 @@
 
   outputs = { self, nixpkgs, ... } @ inputs: {
 
-    nixosConfigurations.nixos-laptop = nixpkgs.lib.nixosSystem {
-      modules = [
-        ./hosts/nixos-laptop/configuration.nix
-      ];
+    nixosConfigurations = {
+
+      nixos-laptop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+
+        specialArgs = { inherit inputs; };
+
+        modules = [
+          ./hosts/nixos-laptop/configuration.nix
+        ];
+      };
+
     };
   };
 }
