1ecaa82bf7ab3720717d38aebad38ddfb03fe1df
ad030 <68517956+ad030@users.noreply.github.com>
2026-05-23 10:08:09 -0400

    reformatted configuration file

--- hosts/nixos-laptop/configuration.nix
+++ hosts/nixos-laptop/configuration.nix
@@ -7,266 +7,258 @@ let
   home-manager = builtins.fetchTarball https://github.com/nix-community/home-manager/archive/release-25.11.tar.gz;
 in
   {
-    imports =
-      [ 
-                        # Include the results of the hardware scan.
-                        /etc/nixos/hardware-configuration.nix
+  imports =
+    [ 
+      # Include the results of the hardware scan.
+      /etc/nixos/hardware-configuration.nix
 
-                        # home manager
-                        (import "${home-manager}/nixos")
-                      ];
+      # home manager
+      (import "${home-manager}/nixos")
+    ];
 
-        # Bootloader.
-        boot = {
-          consoleLogLevel = 3;
-          loader = {
-            systemd-boot.enable = true;
-            efi.canTouchEfiVariables = true;
-          };
+    # Bootloader.
+    boot = {
+      consoleLogLevel = 3;
+      loader = {
+        systemd-boot.enable = true;
+        efi.canTouchEfiVariables = true;
+      };
+    };
+
+    networking.hostName = "nixos-laptop"; # Define your hostname.
+
+
+    # Enable networking
+    networking.networkmanager.enable = true;
+
+    # Set your time zone.
+    time.timeZone = "America/Toronto";
+
+    # Select internationalisation properties.
+    i18n.defaultLocale = "en_CA.UTF-8";
+
+    services = {
+      # Enable the X11 windowing system.
+      # You can disable this if you're only using the Wayland session.
+      xserver = {
+        enable = true;
+        # Configure keymap in X11
+        xkb = {
+          layout = "us";
+          variant = "";
         };
 
-        networking.hostName = "nixos-laptop"; # Define your hostname.
-# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-# Configure network proxy if necessary
-# networking.proxy.default = "http://user:password@proxy:port/";
-# networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-# Enable networking
-              networking.networkmanager.enable = true;
-
-# Set your time zone.
-	time.timeZone = "America/Toronto";
-
-# Select internationalisation properties.
-	i18n.defaultLocale = "en_CA.UTF-8";
-
-	services = {
-		# Enable the X11 windowing system.
-		# You can disable this if you're only using the Wayland session.
-		xserver = {
-			enable = true;
-			# Configure keymap in X11
-			xkb = {
-				layout = "us";
-				variant = "";
-			};
-
-
-                desktopManager = {
-                #   cinnamon.enable = true;
-                #   xfce.enable = true;
-                #   lxqt.enable = true;
-                };
-
-              };
-
-                # Enable touchpad support (enabled default in most desktopManager).
-                libinput.enable = true;
-
-
-                # display manager
-                displayManager = {
-                  ly = {
-                    enable = true;
-                    settings = {
-                      numlock = false;
-                      bigclock = "en";
-                      vimode = true;
-                    };
-                  };
-                  sddm = {
-                    enable = false;
-                    wayland.enable = true;
-                  };
-              };
-
-		# desktop environment
-                # desktopManager = {
-                #   plasma6.enable = true;
-                # };
-
-		# Enable CUPS to print documents.
-		printing.enable = true;
-
-		# Enable sound with pipewire.
-		pulseaudio.enable = false;
-		pipewire = {
-			enable = true;
-			alsa.enable = true;
-			alsa.support32Bit = true;
-			pulse.enable = true;
-			# If you want to use JACK applications, uncomment this
-			#jack.enable = true;
-		};
-		
-		# Enable the OpenSSH daemon.
-                openssh = {
-                  enable = true;
-                  settings.PermitRootLogin = "no";
-                  settings.PasswordAuthentication = false;
-                };
-
-		thermald.enable = true;
-		tlp = {
-			enable = false;
-
-			settings = {
-				CPU_ENERGY_PERF_POLICY_ON_BAT = "powersave";
-				CPU_ENERGY_PERF_POLICY_ON_AC = "powersave";
-
-				START_CHARGE_THRESH_BAT0 = 40; # 40 and below it starts to charge
-				STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
-			};
-		};
-
-		# enable flatpak package manager
-                flatpak.enable = false;
-
-                # secrets service
-                # passSecretService.enable = true;
-                gnome.gnome-keyring.enable = true;
-            };
-
-	# List packages installed in system profile. To search, run:
-	# $ nix search wget
-	environment.systemPackages = with pkgs; [
-                vim
-                wget
-                curl
-                git
-
-		htop
-                fastfetch
-
-                pavucontrol
-                pamixer
-                bluez
-                bluez-tools
-
-                lxqt.pcmanfm-qt
-                papirus-icon-theme
-                feh
-
-		# nix programming; language server, formatter
-		nil
-		nixfmt
-	];
-
-	programs = {
-		neovim = {
-			enable = true;
-			defaultEditor = true;
-		};
-
-		firefox.enable = true;
-
-                # foot terminal emulator
-                foot = { 
-                  enable = true;
-                  enableBashIntegration = true;
-                };
-
-                # sway wl compositor
-                sway = {
-                  enable = true;
-                  xwayland.enable = true;
-                };
-
-                niri.enable = true;
-
-                # gnupg.agent = {
-                #   enable = true;
-                #   enableSSHSupport = true;
-                # };
-	};
-
-	# for pipewire
-        security = {
-          rtkit.enable = true;
-          pam = {
-            services = {
-              swaylock = {};
-            };
-          };
+        desktopManager = {
+        #   cinnamon.enable = true;
+        #   xfce.enable = true;
+        #   lxqt.enable = true;
         };
 
-	nix.settings.experimental-features = [
-		# enable nix flakes
-		"nix-command"
-		"flakes"
-	];
+      };
+
+      # Enable touchpad support (enabled default in most desktopManager).
+      libinput.enable = true;
 
 
-	# Define a user account. Don't forget to set a password with ‘passwd’.
-        users = { 
-          mutableUsers = false;
+      # display manager
+      displayManager = {
+        ly = {
+          enable = true;
+          settings = {
+            numlock = false;
+            bigclock = "en";
+            vimode = true;
+          };
+        };
+      };
 
-          users = {
-		root = {
-			# disable root login
-			hashedPassword = "!";
-		};
-		ad030 = {
-			isNormalUser = true;
-			description = "ad030";
-			extraGroups = [ "networkmanager" "wheel" ];
-			packages = with pkgs; [
-				#  thunderbird
-			];
-			hashedPassword = "$y$j9T$avB97rOQS/qFTosBcYu/w.$1cDcc.hv8V69alJB1vdQ3hGrKIPlJtw.3/OWJPl0Ow9";
-		};
-              };
-            };
+      # desktop environment
+      desktopManager = {
+      #   plasma6.enable = true;
+      };
 
-	# Allow unfree packages
-	nixpkgs.config.allowUnfree = true;
+      # Enable CUPS to print documents.
+      printing.enable = true;
+
+      # Enable sound with pipewire.
+      pulseaudio.enable = false;
+      pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+      };
+
+      # Enable the OpenSSH daemon.
+      openssh = {
+        enable = true;
+        settings.PermitRootLogin = "no";
+        settings.PasswordAuthentication = false;
+      };
+
+      thermald.enable = true;
+      tlp = {
+        enable = false;
+
+        settings = {
+          CPU_ENERGY_PERF_POLICY_ON_BAT = "powersave";
+          CPU_ENERGY_PERF_POLICY_ON_AC = "powersave";
+
+          START_CHARGE_THRESH_BAT0 = 40; # 40 and below it starts to charge
+          STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+        };
+      };
+
+      # enable flatpak package manager
+      flatpak.enable = false;
+
+      # secrets service
+      # passSecretService.enable = true;
+      gnome.gnome-keyring.enable = true;
+    };
+
+    # List packages installed in system profile. To search, run:
+    # $ nix search wget
+    environment.systemPackages = with pkgs; [
+      vim
+      wget
+      curl
+      git
+      bash
+
+      htop
+      fastfetch
+
+      pavucontrol
+      pamixer
+      bluez
+      bluez-tools
+
+      lxqt.pcmanfm-qt
+      papirus-icon-theme
+      feh
+
+      # nix programming; language server, formatter
+      nil
+      nixfmt
+    ];
+
+    programs = {
+      neovim = {
+        enable = true;
+        defaultEditor = true;
+      };
+
+      firefox.enable = true;
+
+      # foot terminal emulator
+      foot = { 
+        enable = true;
+        enableBashIntegration = true;
+      };
+
+      # sway wl compositor
+      sway = {
+        enable = true;
+        xwayland.enable = true;
+      };
+
+      niri.enable = true;
+
+      # gnupg.agent = {
+      #   enable = true;
+      #   enableSSHSupport = true;
+      # };
+    };
+
+    # for pipewire
+    security = {
+      rtkit.enable = true;
+      pam = {
+        services = {
+          swaylock = {};
+        };
+      };
+    };
+
+    nix.settings.experimental-features = [
+      # enable nix flakes
+      "nix-command"
+      "flakes"
+    ];
 
 
-	# Some programs need SUID wrappers, can be configured further or are
-	# started in user sessions.
-	# programs.mtr.enable = true;
+    # Define a user account. Don't forget to set a password with ‘passwd’.
+    users = { 
+      mutableUsers = false;
 
-	# List services that you want to enable:
+      users = {
+        root = {
+          # disable root login
+          hashedPassword = "!";
+        };
+        ad030 = {
+          isNormalUser = true;
+          shell = pkgs.bash;
+          description = "ad030";
+          extraGroups = [ "networkmanager" "wheel" ];
+          packages = with pkgs; [
+                  #  thunderbird
+          ];
+          hashedPassword = "$y$j9T$avB97rOQS/qFTosBcYu/w.$1cDcc.hv8V69alJB1vdQ3hGrKIPlJtw.3/OWJPl0Ow9";
+        };
+      };
+    };
 
-
-	# Open ports in the firewall.
-	# networking.firewall.allowedTCPPorts = [ ... ];
-	# networking.firewall.allowedUDPPorts = [ ... ];
-	# Or disable the firewall altogether.
-	# networking.firewall.enable = false;
-
-	# home manager stuff
-	home-manager.useUserPackages = true;
-	home-manager.useGlobalPkgs = true;
-	home-manager.backupFileExtension = "backup";
-	home-manager.users.ad030 = import ./home.nix;
-
-        # fonts
-        fonts.packages = with pkgs; [
-          noto-fonts
-          noto-fonts-cjk-sans
-          noto-fonts-color-emoji
-
-          font-awesome_4
-          font-awesome_5
-          font-awesome_6
-
-          # nerd fonts
-          nerd-fonts.fira-mono
-          nerd-fonts.meslo-lg
-        ];
-
-	# power management 
-	powerManagement.enable = true;
+    # Allow unfree packages
+    nixpkgs.config.allowUnfree = true;
 
 
-	# This value determines the NixOS release from which the default
-	# settings for stateful data, like file locations and database versions
-	# on your system were taken. It‘s perfectly fine and recommended to leave
-	# this value at the release version of the first install of this system.
-	# Before changing this value read the documentation for this option
-	# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-	system.stateVersion = "25.11"; # Did you read the comment?
+    # Some programs need SUID wrappers, can be configured further or are
+    # started in user sessions.
+    # programs.mtr.enable = true;
 
+    # List services that you want to enable:
+
+    # Open ports in the firewall.
+    # networking.firewall.allowedTCPPorts = [ ... ];
+    # networking.firewall.allowedUDPPorts = [ ... ];
+    # Or disable the firewall altogether.
+    # networking.firewall.enable = false;
+
+        # home manager stuff
+    home-manager = {
+      useUserPackages = true;
+      useGlobalPkgs = true;
+      backupFileExtension = "backup";
+      users.ad030 = import ./home.nix;
+    };
+
+    # fonts
+    fonts.packages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-color-emoji
+
+      font-awesome_4
+      font-awesome_5
+      font-awesome_6
+
+      # nerd fonts
+      nerd-fonts.fira-mono
+      nerd-fonts.meslo-lg
+    ];
+
+    # power management 
+    powerManagement.enable = true;
+
+
+    # This value determines the NixOS release from which the default
+    # settings for stateful data, like file locations and database versions
+    # on your system were taken. It‘s perfectly fine and recommended to leave
+    # this value at the release version of the first install of this system.
+    # Before changing this value read the documentation for this option
+    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+    system.stateVersion = "25.11"; # Did you read the comment?
 }
