b958432c9eaccc56d6909cafa1677808b27e793a
ad030 <68517956+ad030@users.noreply.github.com>
2026-06-12 00:37:11 -0400

    pulled out waybar style.css into separate module, changed tooltip bg color and added border

--- modules/programs/desktop/waybar/default.nix
+++ modules/programs/desktop/waybar/default.nix
@@ -7,9 +7,6 @@
       config,
       ...
     }:
-    let
-      theme = self.themes.gruvbox-dark;
-    in
     {
 
       services = {
@@ -165,51 +162,6 @@
 
           }; # end main bar
         }; # end settings
-
-        style = ''
-          * {
-            border-radius: 0;
-            font-family: MesloLGM Nerd Font;
-            color: ${theme.palette.light1};
-            font-size: 16px;
-          }
-
-          window#waybar {
-            background-color: ${theme.palette.dark1};
-            color: ${theme.palette.light1};
-          }
-
-          #clock,
-          #battery,
-          #cpu,
-          #workspaces,
-          #scratchpad,
-          #memory,
-          #backlight,
-          #wireplumber,
-          #network {
-            background-color: ${theme.palette.dark1};
-            color: ${theme.palette.light1};
-            margin: 0 2px;
-          }
-
-          button:hover {
-            box-shadow: none;
-            text-shadow: none;
-            border-color: transparent;
-            background: none;
-            transition: none;
-          }
-
-          #workspaces button {
-            padding: 0 8px;
-          }
-
-          #workspaces button.focused {
-            background-color: ${theme.palette.dark4};
-            box-shadow: inset 0 -2px ${theme.palette.light1};
-          }
-        ''; # end style
 
       }; # end programs.waybar
     }; # end function
