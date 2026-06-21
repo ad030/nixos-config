4311d16c86f711ddcc4a03a226cb5d949191aae6
ad030 <68517956+ad030@users.noreply.github.com>
2026-06-21 06:55:13 -0400

    hyprland should hopefully work

--- modules/home/desktop/hyprland/default.nix
+++ modules/home/desktop/hyprland/default.nix
@@ -4,22 +4,51 @@
   ...
 }:
 {
-  flake.modules.homeManager.hyprland = {
-    wayland.windowManager.hyprland = {
-      enable = true;
+  flake.modules.homeManager.hyprland =
+    {
+      pkgs,
+      config,
+      lib,
+      ...
+    }:
+    {
+      imports = [
+        self.modules.homeManager.wl-screenshot
+      ];
 
-      # use hyprland package from nixos system
-      package = null;
-      portalPackage = null;
+      wayland.windowManager.hyprland = {
+        enable = true;
 
-      settings = {
-        "$mod" = "SUPER";
+        # use hyprland package from nixos system
+        package = null;
+        portalPackage = null;
 
-        bind = [
-          "$mod, D, exec, fuzzel"
-          "$mod, ENTER, exec, foot"
-        ];
+        settings = {
+          bind =
+            let
+              mod = "SUPER";
+              mkBind = keys: command: "hl.bind(${keys}, ${command})";
+              exec_cmd = command: "hl.dsp.exec_cmd(${command})";
+            in
+            [
+              (mkBind "${mod} + D" (exec_cmd "${lib.getExe pkgs.fuzzel}"))
+              (mkBind "${mod} + ENTER" (exec_cmd "${lib.getExe pkgs.foot}"))
+              (mkBind "${mod} + SHIFT + S" (exec_cmd "wl-screenshot"))
+
+              (mkBind "${mod} + XF86MonBrightnessDown" (exec_cmd "${lib.getExe pkgs.brightnessctl} set 4%-"))
+              (mkBind "${mod} + XF86MonBrightnessUp" (exec_cmd "${lib.getExe pkgs.brightnessctl} set +4%"))
+
+              (mkBind "${mod} + XF86AudioMute" (
+                exec_cmd "${pkgs.wireplumber}/bin/wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+              ))
+              (mkBind "${mod} + XF86AudioRaiseVolume" (
+                exec_cmd "${pkgs.wireplumber}/bin/wpctl set-volume @DEFAULT_AUDIO_SINK@ 4%+"
+              ))
+              (mkBind "${mod} + XF86AudioLowerVolume" (
+                exec_cmd "${pkgs.wireplumber}/bin/wpctl set-volume @DEFAULT_AUDIO_SINK@ 4%-"
+              ))
+            ];
+        };
       };
     };
-  };
 }
