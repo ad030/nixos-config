9533e0782e77c885ac5874c2ca36944a1c15ae76
ad030 <68517956+ad030@users.noreply.github.com>
2026-06-26 06:03:13 -0400

    set root user password in case i accidentally mess up sops-nix and cannot log in

--- modules/nixos/core/users-settings.nix
+++ modules/nixos/core/users-settings.nix
@@ -2,6 +2,9 @@
   flake.modules.nixos.users-settings = {
     users.mutableUsers = false;
 
+    # root user only for emergency logins (if i mess up sops-nix and passwords are disabled)
+    users.users.root.hashedPassword = "$y$j9T$G8vUPuF8xgGGP8Y/CbP9x.$jzOR575KBP/R/PT9baCSmvRuqzhQw3rb88xKTijHkF8";
+
     # create user and group for use in shared media directories (nfs)
     users.users.media = {
       uid = 3333;
