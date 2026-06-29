0d6618ad4e0b0ad0d2d0ecb42855b42da5901440
ad030 <68517956+ad030@users.noreply.github.com>
2026-06-29 08:47:25 -0400

    added authorized ssh keys to root user

--- modules/nixos/core/users-settings.nix
+++ modules/nixos/core/users-settings.nix
@@ -2,8 +2,14 @@
   flake.modules.nixos.users-settings = {
     users.mutableUsers = false;
 
-    # root user only for emergency logins (if i mess up sops-nix and passwords are disabled)
-    users.users.root.hashedPassword = "$y$j9T$KPQS8q1qZ8.HR5rtnT33N0$jlOkZFzUl4tWpKXBzreJcbe7cQW1E0JPL9.2uOKp0u.";
+    # root user only for emergency logins
+    # and ssh to rebuild systems
+    users.users.root = {
+      hashedPassword = "$y$j9T$KPQS8q1qZ8.HR5rtnT33N0$jlOkZFzUl4tWpKXBzreJcbe7cQW1E0JPL9.2uOKp0u.";
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOToXINio9+fZSsAW3/YmgioP+7RLFXwEZNJRWRQMZjl solanum@brittle-hollow"
+      ];
+    };
 
     # create user and group for use in shared media directories (nfs)
     users.users.media = {
