cd480bb3eb86a366e91cd9fe4db35e2b6e81b786
ad030 <68517956+ad030@users.noreply.github.com>
2026-06-30 05:39:02 -0400

    it's an ntfs drive not an ext4 drive i forgot and now i fixed it and it mounts properly

--- modules/hosts/attlerock/_nixos/mounts.nix
+++ modules/hosts/attlerock/_nixos/mounts.nix
@@ -1,11 +1,11 @@
 {
+  fileSystems."/srv/media/tank" = {
+    device = "tank";
+    fsType = "zfs";
+  };
+
   fileSystems."/srv/media/wd-blue-1tb" = {
     device = "/dev/disk/by-uuid/35EFA427397E8837";
-    fsType = "ext4";
-    options = [
-      "defaults"
-      "nofail"
-      "exec"
-    ];
+    fsType = "ntfs3";
   };
 }
