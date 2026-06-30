66b3f5a5d53d2ba264fa0bad48185a539cec3d8f
ad030 <68517956+ad030@users.noreply.github.com>
2026-06-30 05:15:42 -0400

    fixed incorrect option name from deviceType to fsType because i'm a moron

--- modules/hosts/attlerock/_nixos/mounts.nix
+++ modules/hosts/attlerock/_nixos/mounts.nix
@@ -1,7 +1,7 @@
 {
   fileSystems."/srv/media/wd-blue-1tb" = {
     device = "/dev/disk/by-uuid/35EFA427397E8837";
-    deviceType = "ext4";
+    fsType = "ext4";
     options = [
       "defaults"
       "nofail"
