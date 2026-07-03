f729e2db8dbefb46bbace8c37f986d154221da01
ad030 <68517956+ad030@users.noreply.github.com>
2026-07-03 07:14:36 -0400

    added fix for steam black window on niri with xwayland-satellite

--- modules/nixos/gaming/steam.nix
+++ modules/nixos/gaming/steam.nix
@@ -6,8 +6,20 @@
       ...
     }:
     {
+      # steam on niri with xwayland-satellite spawns black square
+      # fix is to add this flag
+      # https://discourse.nixos.org/t/niri-xwayland-satellite-black-steam-window-fix/77107
+      nixpkgs.overlays = [
+        (final: prev: {
+          steam = prev.steam.override {
+            extraArgs = "-cef-disable-gpu-compositing";
+          };
+        })
+      ];
+
       programs.steam = {
         enable = true;
+        protontricks.enable = true;
       };
     };
 }
