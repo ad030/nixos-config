9428364bfb01b588c5c8daf36074432d8198edf9
ad030 <68517956+ad030@users.noreply.github.com>
2026-07-04 05:41:48 -0400

    removed unused media user

--- modules/nixos/core/users-settings.nix
+++ modules/nixos/core/users-settings.nix
@@ -11,12 +11,7 @@
       ];
     };
 
-    # create user and group for use in shared media directories (nfs)
-    users.users.media = {
-      uid = 3333;
-      group = "media";
-      isSystemUser = true;
-    };
+    # create group for use in shared media directories (nfs)
     users.groups.media.gid = 3333;
   };
 }
