ab610c367949202b5506a9190ea74a76bc4d522f
ad030 <68517956+ad030@users.noreply.github.com>
2026-07-05 06:15:15 -0400

    added more to nvidia module, will see how well it works

--- modules/nixos/nvidia/default.nix
+++ modules/nixos/nvidia/default.nix
@@ -8,12 +8,36 @@
     {
       pkgs,
       config,
+      ...
     }:
     {
+      boot.initrd.availableKernelModules = [
+        "nvidia_drm"
+        "nvidia_modeset"
+        "nvidia"
+      ];
+
+      environment.sessionVariables = {
+        NIXOS_OZONE_WL = "1";
+      };
+
       services.xserver.videoDrivers = [ "nvidia" ];
-      # environment.systemPackages = [ pkgs.nvtopPackages.nvidia ];
+      environment.systemPackages = with pkgs; [ nvtopPackages.nvidia ];
+
       hardware.graphics.enable = true;
-      hardware.nvidia.open = true;
+
+      hardware.nvidia = {
+        # kernel modesetting needed for wayland
+        modesetting.enable = true;
+        # false = use open drivers from nvidia (not nouveau)
+        open = true;
+
+        powerManagement.enable = false;
+        powerManagement.finegrained = false;
+
+      };
+
       programs.sway.package = pkgs.sway.override { extraOptions = [ "--unsupported-gpu" ]; };
+
     };
 }
