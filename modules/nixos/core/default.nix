66d7b94f7d9adfabb886b02e2afac35ae44912cb
ad030 <68517956+ad030@users.noreply.github.com>
2026-07-23 00:00:17 -0400

    moved package installations into default.nix file

--- modules/nixos/core/default.nix
+++ modules/nixos/core/default.nix
@@ -5,22 +5,55 @@
   ...
 }:
 {
-  flake.modules.nixos.core = {
-    imports = with config.flake.modules.nixos; [
-      bootloader
-      network
-      package-caches
-      nix-settings
-      core-packages
-      fonts
-      users-settings
-      ssh
-      security
-      secret-service
-      sops
-      filesystem
-      power-management
-      printing
-    ];
-  };
+  flake.modules.nixos.core =
+    {
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
+    {
+      imports = with config.flake.modules.nixos; [
+        bootloader
+        network
+        package-caches
+        nix-settings
+        fonts
+        users-settings
+        ssh
+        security
+        secret-service
+        sops
+        filesystem
+        power-management
+        printing
+      ];
+
+      programs = {
+        neovim = {
+          enable = true;
+          defaultEditor = true;
+        };
+      };
+
+      environment.systemPackages = with pkgs; [
+        neovim
+        wget
+        curl
+        git
+        bash
+        python314
+        jq
+        htop
+
+        # notifications
+        libnotify
+        mako
+
+        # nix programming; language server, formatter
+        nil
+        nixfmt
+      ];
+
+    };
 }
