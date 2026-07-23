85651886b9cd292db31f489a0eac8e69d519beaa
ad030 <68517956+ad030@users.noreply.github.com>
2026-07-23 07:28:38 -0400

    added extra comment explaining purpose of ssl root certificate module

--- modules/nixos/ssl-root-certs.nix
+++ modules/nixos/ssl-root-certs.nix
@@ -5,6 +5,7 @@
 }:
 {
   # trusted public ssl root certificates
+  # needed for https connections to my homelab
   flake.modules.nixos.ssl-root-certs = {
     security.pki.certificateFiles = [
       (self + "/certs/rootCA.pem")
