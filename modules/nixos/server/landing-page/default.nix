2e094deb27301f51058e0309aae15de51c053d1b
ad030 <68517956+ad030@users.noreply.github.com>
2026-07-25 07:18:29 -0400

    updated landing page

--- modules/nixos/server/landing-page/default.nix
+++ modules/nixos/server/landing-page/default.nix
@@ -17,18 +17,38 @@
         };
       };
 
+      networking.firewall = {
+        allowedTCPPorts = [
+          80
+          443
+          8080
+        ];
+      };
+
       services.nginx.virtualHosts = {
         "home.lan" = {
           default = true;
 
           locations."/".index = "index.html";
+
           root = "/etc/www";
+
+          listen = [
+            {
+              addr = "0.0.0.0";
+              port = 80;
+            }
+            {
+              addr = "0.0.0.0";
+              port = 443;
+              ssl = true;
+            }
+          ];
 
           forceSSL = true;
           sslCertificate = "/etc/nginx/ssl/homelab-domain.pem";
           sslCertificateKey = "/etc/nginx/ssl/homelab-domain-key.pem";
         };
       };
-
     };
 }
