df08d4c30ae52601f6cae2dedbf57a9fd76664ce
ad030 <68517956+ad030@users.noreply.github.com>
2026-07-30 07:55:58 -0400

    added acl settings for nfs drive

--- modules/hosts/attlerock/_nixos/nfs.nix
+++ modules/hosts/attlerock/_nixos/nfs.nix
@@ -23,6 +23,38 @@
     "a /export/tank - - - - default:mask::rwx"
   ];
 
+  systemd.tmpfiles.settings."export-tank" = {
+    "/export/tank" = {
+      d = {
+        user = "root"; 
+        group = "media"; 
+        mode = "2775"; 
+      };
+      # ensure media group gets rwx permissions
+      "a+media-perms" = {
+        type = "a";
+        argument = "group:media:rwx";
+      };
+      # set default permissions for new files (owner, group, other)
+      "a+user-perms" = {
+        type = "a";
+        argument = "default:user::rwx";
+      };
+      "a+group-perms" = {
+        type = "a";
+        argument = "default:group:media:rwx";
+      };
+      "a+other-perms" = {
+        type = "a";
+        argument = "default:other::rwx";
+      };
+      # set default acl mask
+      "a+default-mask" = {
+        type = "a";
+        argument = "default:mask::rwx";
+      };
+  };
+
   services.nfs.server.exports = ''
     /export  192.168.8.0/24(ro,fsid=0,no_subtree_check)
     /export/tank 192.168.8.0/24(rw,nohide,no_subtree_check,root_squash)
