71aa14a8070e099289c5869a98f28140e232f08c
ad030 <68517956+ad030@users.noreply.github.com>
2026-09-12 19:58:25 -0400

    set home-manager flake input to follow stable nixpkgs

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
 
     home-manager = {
       url = "github:nix-community/home-manager/master";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     # declarative flatpak installation
