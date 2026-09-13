936af3fedd7e7b62e4177e47fae4891b5a77fc80
ad030 <68517956+ad030@users.noreply.github.com>
2026-09-13 06:13:24 -0400

    removed commented out tailscale serve blocks

--- modules/nixos/server/slskd/default.nix
+++ modules/nixos/server/slskd/default.nix
@@ -60,15 +60,6 @@
         allowedTCPPorts = ports.tcp;
       };
 
-      # services.tailscale.serve.services = {
-      #   slskd = {
-      #     advertised = true;
-      #     endpoints = {
-      #       "tcp:5030" = "http://10.0.0.3:5030";
-      #     };
-      #   };
-      # };
-
       sops.secrets."slskd/env" = { };
 
       containers.slskd = {
