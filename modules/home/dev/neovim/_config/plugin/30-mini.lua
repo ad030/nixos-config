3c53720d9f32c2d3857290c97626ac11348f360e
ad030 <68517956+ad030@users.noreply.github.com>
2026-09-21 12:46:01 -0400

    nvim: replaced individual mini.nvim package imports with the entire library import

--- modules/home/dev/neovim/_config/plugin/30-mini.lua
+++ modules/home/dev/neovim/_config/plugin/30-mini.lua
@@ -1,18 +1,13 @@
 vim.pack.add({
-	"https://github.com/nvim-mini/mini.ai",
-	"https://github.com/nvim-mini/mini.completion",
-	"https://github.com/nvim-mini/mini.icons",
 	{
-		src = "https://github.com/nvim-mini/mini.pick",
+		src = "https://github.com/nvim-mini/mini.nvim",
 		version = "stable",
 	},
-	"https://github.com/nvim-mini/mini.snippets",
-	"https://github.com/nvim-mini/mini.statusline",
-	"https://github.com/nvim-mini/mini.surround",
 })
 
 require("mini.ai").setup({ n_lines = 500 })
 require("mini.completion").setup()
+require("mini.cmdline").setup()
 require("mini.icons").setup()
 require("mini.pick").setup()
 require("mini.snippets").setup()
