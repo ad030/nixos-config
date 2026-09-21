bce09a73243836641fec1c7502e118e6f350f1e0
ad030 <68517956+ad030@users.noreply.github.com>
2026-09-21 12:45:09 -0400

    nvim: updated obsidian.nvim config

--- modules/home/dev/neovim/_config/after/ftplugin/markdown.lua
+++ modules/home/dev/neovim/_config/after/ftplugin/markdown.lua
@@ -13,6 +13,7 @@ require("render-markdown").setup({})
 local obsidian = require("obsidian")
 
 obsidian.setup({
+	legacy_commands = false, -- this can be removed in v4.0.0
 	workspaces = {
 		{
 			name = "personal",
@@ -24,8 +25,10 @@ obsidian.setup({
 	new_notes_location = "notes_subdir",
 
 	daily_notes = {
+		enabled = true,
 		folder = "000_inbox",
 		date_format = "%Y-%m-%d",
+		template = "daily.md",
 		default_tags = { "daily" },
 	},
 
@@ -36,52 +39,69 @@ obsidian.setup({
 	},
 
 	attachments = {
-		img_folder = "900_assets",
+		folder = "900_assets",
 	},
 
-	mappings = {
-		-- [G]oto [F]ile in obsidian vault
-		["gf"] = {
-			action = function()
-				return obsidian.util.gf_passthrough()
-			end,
-			opts = { noremap = false, expr = true, buffer = true },
-		},
-		-- Toggle check-boxes.
-		["<leader>ch"] = {
-			action = function()
-				return obsidian.util.toggle_checkbox()
-			end,
-			opts = { buffer = true, desc = "Obsidian: Toggle [ch]eckbox" },
-		},
-		-- smart action; toggle checkbox or follow link
-		["<cr>"] = {
-			action = function()
-				return obsidian.util.smart_action()
-			end,
-			opts = { buffer = true, expr = true },
-		},
-		-- open daily note
-		["<leader>Od"] = {
-			action = function()
-				return obsidian.client.today()
-			end,
-			opts = { buffer = true, expr = true, desc = "[O]bsidian: Open [D]aily Note" },
-		},
-		-- open new note
-		["<leader>On"] = {
-			action = function()
-				return obsidian.client.today()
-			end,
-			opts = { buffer = true, expr = true, desc = "[O]bsidian: Open [N]ew Note" },
-		},
-	},
-
-	completion = {
-		nvim_cmp = false,
-	},
+	-- mappings = {
+	-- 	-- [G]oto [F]ile in obsidian vault
+	-- 	["gf"] = {
+	-- 		action = function()
+	-- 			return obsidian.util.gf_passthrough()
+	-- 		end,
+	-- 		opts = { noremap = false, expr = true, buffer = true },
+	-- 	},
+	-- 	-- Toggle check-boxes.
+	-- 	["<leader>ch"] = {
+	-- 		action = function()
+	-- 			return obsidian.util.toggle_checkbox()
+	-- 		end,
+	-- 		opts = { buffer = true, desc = "Obsidian: Toggle [ch]eckbox" },
+	-- 	},
+	-- 	-- smart action; toggle checkbox or follow link
+	-- 	["<cr>"] = {
+	-- 		action = function()
+	-- 			return obsidian.util.smart_action()
+	-- 		end,
+	-- 		opts = { buffer = true, expr = true },
+	-- 	},
+	-- 	-- open daily note
+	-- 	["<leader>Od"] = {
+	-- 		action = function()
+	-- 			return obsidian.client.today()
+	-- 		end,
+	-- 		opts = { buffer = true, expr = true, desc = "[O]bsidian: Open [D]aily Note" },
+	-- 	},
+	-- 	-- open new note
+	-- 	["<leader>On"] = {
+	-- 		action = function()
+	-- 			return obsidian.client.today()
+	-- 		end,
+	-- 		opts = { buffer = true, expr = true, desc = "[O]bsidian: Open [N]ew Note" },
+	-- 	},
+	-- },
 
 	picker = {
 		name = "mini.pick",
 	},
 })
+
+-- subcommand completion
+-- https://github.com/obsidian-nvim/obsidian.nvim/wiki/Commands
+vim.api.nvim_create_autocmd("CmdlineChanged", {
+	callback = function()
+		local cmdline = vim.fn.getcmdline()
+		if vim.fn.getcmdtype() ~= ":" then
+			return
+		end
+		if not cmdline:match("^Obsidian[A-Za-z0-9]*$") then
+			return
+		end
+		vim.fn.wildtrigger()
+	end,
+})
+
+vim.keymap.set("n", "<leader>On", function()
+	return obsidian.actions.new()
+end, { desc = "Obsidian: Open new note" })
+
+vim.keymap.set("n", "<leader>Od", "<cmd>Obsidian today<CR>", { desc = "Obsidian: Open daily note" })
