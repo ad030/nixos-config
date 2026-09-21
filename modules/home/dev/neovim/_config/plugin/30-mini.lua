466e703c3b4baae35aec7fe5524162150269ed03
ad030 <68517956+ad030@users.noreply.github.com>
2026-09-21 12:01:43 -0400

    nvim: added obsidian.nvim, mini.pick plugin; added number to oil plugin file

--- modules/home/dev/neovim/_config/plugin/30-mini.lua
+++ modules/home/dev/neovim/_config/plugin/30-mini.lua
@@ -2,6 +2,10 @@ vim.pack.add({
 	"https://github.com/nvim-mini/mini.ai",
 	"https://github.com/nvim-mini/mini.completion",
 	"https://github.com/nvim-mini/mini.icons",
+	{
+		src = "https://github.com/nvim-mini/mini.pick",
+		version = "stable",
+	},
 	"https://github.com/nvim-mini/mini.snippets",
 	"https://github.com/nvim-mini/mini.statusline",
 	"https://github.com/nvim-mini/mini.surround",
@@ -10,6 +14,7 @@ vim.pack.add({
 require("mini.ai").setup({ n_lines = 500 })
 require("mini.completion").setup()
 require("mini.icons").setup()
+require("mini.pick").setup()
 require("mini.snippets").setup()
 require("mini.statusline").setup({
 	use_icons = vim.g.have_nerd_font,
