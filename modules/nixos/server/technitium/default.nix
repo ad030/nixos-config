e38286406a03ec694da040e5fcec70ccf5e5f8da
ad030 <68517956+ad030@users.noreply.github.com>
2026-09-23 01:28:22 -0400

    tailscale network works poorly with the dns server inside a private networked container; just run technitium as a service on the host instead

--- modules/nixos/server/technitium/default.nix
+++ modules/nixos/server/technitium/default.nix
@@ -3,24 +3,19 @@
   flake.modules.nixos.technitium =
     { config, lib, ... }:
     let
-      ports = {
-        tcp = [
-          53 # dns
-          5380 # web ui
-          53443 # web ui https
-        ];
-        udp = [
-          53 # dns
-        ];
-      };
-
-      localAddr = "10.0.0.10";
+      localAddr = "127.0.0.1";
       webPort = "5380";
     in
     {
       networking.firewall = {
-        allowedTCPPorts = ports.tcp;
-        allowedUDPPorts = ports.udp;
+        allowedTCPPorts = [
+          53
+          5380
+          53443
+        ];
+        allowedUDPPorts = [
+          53
+        ];
       };
 
       services.nginx.virtualHosts = {
@@ -36,60 +31,16 @@
         };
       };
 
-      containers.technitium = {
-        autoStart = true;
+      services.technitium-dns-server = {
+        enable = true;
 
-        privateNetwork = true;
-        hostAddress = "10.0.0.1";
-        localAddress = localAddr;
+        openFirewall = false;
+      };
 
-        privateUsers = "pick";
-
-        forwardPorts =
-          map (p: {
-            hostPort = p;
-            protocol = "tcp";
-          }) ports.tcp
-          ++ map (p: {
-            hostPort = p;
-            protocol = "udp";
-          }) ports.udp;
-
-        config =
-          {
-            config,
-            lib,
-            pkgs,
-            ...
-          }:
-          {
-            services.technitium-dns-server = {
-              enable = true;
-
-              openFirewall = false;
-            };
-
-            # they messed up the hardening and technitium can't even write to its own log file
-            # https://discourse.nixos.org/t/technitium-dns-fails-with-access-denied-at-var-lib/64672/4
-            systemd.services.technitium-dns-server.serviceConfig = {
-              LogsDirectory = "technitium";
-            };
-
-            networking.firewall = {
-              allowedTCPPorts = ports.tcp;
-              allowedUDPPorts = ports.udp;
-            };
-
-            networking.useHostResolvConf = lib.mkForce false;
-            services.resolved = {
-              enable = true;
-
-              # needs to be set in order to access port 53
-              settings.Resolve.DNSStubListener = "no";
-            };
-
-            system.stateVersion = "26.05";
-          };
+      # they messed up the hardening and technitium can't even write to its own log file
+      # https://discourse.nixos.org/t/technitium-dns-fails-with-access-denied-at-var-lib/64672/4
+      systemd.services.technitium-dns-server.serviceConfig = {
+        LogsDirectory = "technitium";
       };
     };
 
